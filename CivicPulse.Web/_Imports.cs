741a055b8e392d1cdf5664c69cc2c798fef03ebb
Rabiya <rabiya@civicpulse.pk>
2026-05-25 17:26:03 +0500

    Day 5: Admin dashboards and management
    - Admin dashboard with statistics and charts
    - Complaint management with filtering/status updates
    - Complaint detail view for admins
    - Status badge component
    - Landing page and global imports

---
+++ CivicPulse.Web/_Imports.cs
@@ -0,0 +1,9 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Forms;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Microsoft.AspNetCore.Authorization;
